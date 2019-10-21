84c00faed597bcd91ece5e233890d7bb475faf99
Ángel Z <angel4441916@gmail.com>
2019-10-20 21:15:06 -0400

    database name changed

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/simplejwt', {
+mongoose.connect('mongodb://localhost/qmsDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 
